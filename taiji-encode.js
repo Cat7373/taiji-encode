9cbba26e383a390999ed99605fc35251a1425312
Cat73 <root@cat73.org>
2023-08-10 15:50:20 +0800

    :memo: 完善代码文档

--- taiji-encode.js
+++ taiji-encode.js
@@ -1,6 +1,17 @@
+/**
+ * 六十四卦的每个字符，加代表 Base64 等于号的太极图
+ */
 const guaChs = '䷁䷗䷆䷒䷎䷣䷭䷊䷏䷲䷧䷵䷽䷶䷟䷡䷇䷂䷜䷻䷦䷾䷯䷄䷬䷐䷮䷹䷞䷰䷛䷪䷖䷚䷃䷨䷳䷕䷑䷙䷢䷔䷿䷥䷷䷝䷱䷍䷓䷩䷺䷼䷴䷤䷸䷈䷋䷘䷅䷉䷠䷌䷫䷀☯'
+/**
+ * Base64 的字符映射表
+ */
 const base64Chs = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
 
+/**
+ * 太极编码
+ * @param {String} str 被编码的字符串
+ * @returns 编码后的太极字符串
+ */
 export function taijiEncode(str) {
   let base64 = btoa(str)
   let taiji = base64
@@ -12,6 +23,11 @@ export function taijiEncode(str) {
   return taiji
 }
 
+/**
+ * 太极解码
+ * @param {String} taiji 被解码的太极字符串
+ * @returns 解码后的字符串
+ */
 export function taijiDecode(taiji) {
   let base64 = taiji
 
