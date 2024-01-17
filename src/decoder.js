907346a7f1bf25344b31458defa481c277cad5b0
Cat73 <root@cat73.org>
2024-01-17 11:38:10 +0800

    :sparkles: 添加参数以支持输入输出 Base64 内容，以适应特殊场景
    Fix #2

--- src/decoder.js
+++ src/decoder.js
@@ -4,15 +4,15 @@ import { base64Chs, taijiChs } from './mapping.js'
 /**
  * 太极解码
  * @param {String} taiji 被解码的太极字符串
+ * @param {Boolean} base64 指示解码后无需进行 Base64 解码，直接输出 Base64 的内容
  * @returns 解码后的字符串
  */
-export function taijiDecode(taiji) {
-  let base64 = taiji
+export function taijiDecode(taiji, { base64=false }={}) {
+  let str = taiji
 
   for (let i = 0; i < 65; i++) {
-    base64 = base64.replaceAll(taijiChs[i], base64Chs[i])
+    str = str.replaceAll(taijiChs[i], base64Chs[i])
   }
 
-  const str = decode(base64)
-  return str
+  return base64 ? str : decode(base64)
 }
--- src/encoder.js
+++ src/encoder.js
@@ -4,11 +4,11 @@ import { base64Chs, taijiChs } from './mapping.js'
 /**
  * 太极编码
  * @param {String} str 被编码的字符串
+ * @param {Boolean} base64 指示输入内容已经通过 Base64 编码过，无需再次编码
  * @returns 编码后的太极字符串
  */
-export function taijiEncode(str) {
-  const base64 = encode(str)
-  let taiji = base64
+export function taijiEncode(str, { base64=false }={}) {
+  let taiji = base64 ? str : encode(str)
 
   for (let i = 0; i < 65; i++) {
     taiji = taiji.replaceAll(base64Chs[i], taijiChs[i])
